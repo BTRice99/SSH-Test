13582d5df23c9ba94a277a627bb40854dcfa83b8
BTRice99 <51234898+BTRice99@users.noreply.github.com>
2021-01-08 09:22:16 -0500

    Update gitTestGit.py

--- gitTestGit.py
+++ gitTestGit.py
@@ -1,3 +1,9 @@
-f = open("gitOutput.txt", "a")
-f.write(str("It Worked!!!!"))
-f.close()
+import time
+
+x = 6
+
+while 6 > 3:
+  f = open("gitOutput.txt", "a")
+  f.write(str("It Worked Again!!!!"))
+  f.close()
+  time.sleep(15)
